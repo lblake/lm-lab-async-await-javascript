19780e5299dfcf20527ec07762e62126f34593f0
lblake <treleven.lloyd@gmail.com>
2023-01-30 13:52:36 +0000

    completed first exercise

--- exercise001.js
+++ exercise001.js
@@ -1,14 +1,50 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  // Generate random number between 0 and 9
+  const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
+  if (randomInt % 2 === 0) {
+    // Success
+    setTimeout(() => resolve('done'), 2000);
+  } else {
+    // Failure
+    setTimeout(() => reject('error'), 2000);
+  }
 });
 
 // Your solution(s) to exercise001 here!
+
+/*Task 1 solution */
+
+promise
+  .then((response) => {
+    console.log(`Yay! Promise resolved with response: ${response}`);
+  })
+  .catch((error) => {
+    console.log(`Boo. Promise rejected with response: ${error}`);
+  });
+
+/* Task 2 solution */
+
+const asyncPromise = async () => {
+  try {
+    const response = await promise;
+    if (response) {
+      console.log(`Yay! Promise resolved with response: ${response}`);
+    }
+  } catch (error) {
+    console.log(`Boo. Promise rejected with response: ${error}`);
+  }
+};
+
+asyncPromise();
+
+/* Top level await*/
+
+try {
+  const topLevel = await promise;
+  if (response) {
+    console.log(`Yay! Promise resolved with response: ${topLevel}`);
+  }
+} catch (error) {
+  console.log(`Boo. Promise rejected with response: ${error}`);
+}
