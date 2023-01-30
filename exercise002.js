a5a26c629dc11f7e337538f6f4d6c8b66133858a
lblake <treleven.lloyd@gmail.com>
2023-01-30 14:35:01 +0000

    completed second exercise

--- exercise002.js
+++ exercise002.js
@@ -1,12 +1,35 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+/* This API endpoint failed as I had not signed-up but use to check what happens if the endpoint failed*/
+const recipe =
+  'https://api.spoonacular.com/food/ingredients/9266/information?amount=1';
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+const lukeSkyWalker = 'http swapi.dev/api/people/1/';
+
+const jsonTypicode = 'https://jsonplaceholder.typicode.com/todos/1';
+
+async function fetchFailed() {
+  const results = await fetch(recipe);
+  const data = await results.json();
+  console.log(data);
+}
+fetchFailed();
+
+async function fetchStarWarsCharacter() {
+  const results = await fetch('https://swapi.dev/api/people/1/');
+  const data = await results.json();
+  console.log(data);
+}
+fetchStarWarsCharacter();
+
+const fetchData = async () => {
+  try {
+    let response = await fetch(jsonTypicode);
+    let data = await response.json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 fetchData(jsonTypicode);
